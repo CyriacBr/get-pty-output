3423b0f1dec6202fa89f94377b0b42759cb0f570
Béré Cyriac <cyriacbr@gmail.com>
2021-12-17 21:38:23 +0000

    chore: fix CI for windows hosts

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -11,45 +11,51 @@ test('binding exists', async (t) => {
   t.ok(execSync)
 })
 
-test('output can be captured', async (t) => {
-  const res = await exec('echo hey')
-  t.ok(res.output.includes('hey'))
-})
+/**
+ * During CI, tty isn't available, and there's no way
+ * around that for windows hosts
+ */
+if (!process.env.WINDOWS_CI) {
+  test('output can be captured', async (t) => {
+    const res = await exec('echo hey')
+    t.ok(res.output.includes('hey'))
+  })
 
-test('exec is non-blocking', (t) => {
-  let i = 0
-  exec('echo hey').then(() => {
-    i++
+  test('exec is non-blocking', (t) => {
+    let i = 0
+    exec('echo hey').then(() => {
+      i++
+      t.end()
+    })
+    t.equal(i, 0)
+  })
+
+  test('execSync is blocking', (t) => {
+    const res = execSync('node -e "setTimeout(()=>{console.log(\'hey\')}, 2000)"')
+    t.ok(res.output.includes('hey'))
     t.end()
   })
-  t.equal(i, 0)
-})
 
-test('execSync is blocking', (t) => {
-  const res = execSync('node -e "setTimeout(()=>{console.log(\'hey\')}, 2000)"')
-  t.ok(res.output.includes('hey'))
-  t.end()
-})
+  test('colors are captured', async (t) => {
+    const res = await exec('node -e console.log(100)')
+    t.is(res.output.trim(), formatWithOptions({ colors: true }, 100))
+  })
 
-test('colors are captured', async (t) => {
-  const res = await exec('node -e console.log(100)')
-  t.is(res.output.trim(), formatWithOptions({ colors: true }, 100))
-})
+  test('cwd option works', async (t) => {
+    const res = await exec('node ./simple-cmd', { cwd })
+    t.true(res.output.includes('hello world'))
+  })
 
-test('cwd option works', async (t) => {
-  const res = await exec('node ./simple-cmd', { cwd })
-  t.true(res.output.includes('hello world'))
-})
+  test('timeout works', async (t) => {
+    const res = await exec('node ./long-running-cmd', { timeout: 2, cwd })
+    t.is(res.truncated, true)
+  })
 
-test('timeout works', async (t) => {
-  const res = await exec('node ./long-running-cmd', { timeout: 2, cwd })
-  t.is(res.truncated, true)
-})
-
-test('errors are handled', async (t) => {
-  try {
-    await exec('node ./doesnt-exist', { cwd })
-  } catch (error) {
-    t.ok(error)
-  }
-})
+  test('errors are handled', async (t) => {
+    try {
+      await exec('node ./doesnt-exist', { cwd })
+    } catch (error) {
+      t.ok(error)
+    }
+  })
+}
